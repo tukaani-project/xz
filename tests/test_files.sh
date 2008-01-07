908b2ac604b9940369d7fe8a45e9eb6da5d2a24c
Lasse Collin <lasse.collin@tukaani.org>
2008-01-07 13:49:19 +0200

    Added test_files.sh to test decoding of the files in the tests/files directory. It doesn't test the malicious files yet.

---
+++ tests/test_files.sh
@@ -0,0 +1,40 @@
+#/bin/sh
+
+###############################################################################
+#
+#   Copyright (C) 2008 Lasse Collin
+#
+#   This library is free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Lesser General Public License for more details.
+#
+###############################################################################
+
+for I in "$srcdir/files/"good-*
+do
+	if ! ../src/lzmadec/lzmadec "$I" > /dev/null 2> /dev/null ; then
+		echo "Good file failed: $I"
+		(exit 1)
+		exit 1
+	fi
+done
+
+for I in "$srcdir/files/"bad-*
+do
+	if ../src/lzmadec/lzmadec "$I" > /dev/null 2> /dev/null ; then
+		echo "Bad file succeeded: $I"
+		(exit 1)
+		exit 1
+	fi
+done
+
+# TODO: Test malicious files too, but with care.
+
+(exit 0)
+exit 0

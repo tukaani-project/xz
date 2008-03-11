tukaani-project/xz

commit 1a3b21859818e4d8e89a1da99699233c1bfd197d

Don't memzero() the history buffer when initializing LZ decoder. There's no danger of information leak here, so it isn't required. Doing memzero() takes a lot of time with large dictionaries, which could make it easier to construct DoS attack to consume too much CPU time.

src/liblzma/lz/lz_decoder.c
@@ -429,10 +429,9 @@ lzma_lz_decoder_reset(lzma_lz_decoder *lz, lzma_allocator *allocator,
 			return LZMA_MEM_ERROR;
 	}
 
-	// Clean up the buffers to make it very sure that there are
-	// no information leaks when multiple steams are decoded
-	// with the same decoder structures.
-	memzero(lz->dict, dict_real_size);
+	// Clean up the temporary buffer to make it very sure that there are
+	// no information leaks when multiple steams are decoded with the
+	// same decoder structures.
 	memzero(lz->temp, LZMA_BUFFER_SIZE);
 
 	// Reset the variables so that lz_get_byte(lz, 0) will return '\0'.

commit f310c50286d9e4e9c6170bb65348c9bb430a65b4

Initialize the last byte of the dictionary to zero so that lz_get_byte(lz, 0) returns zero. This was broken by 1a3b21859818e4d8e89a1da99699233c1bfd197d.

src/liblzma/lz/lz_decoder.c
@@ -433,6 +433,7 @@ lzma_lz_decoder_reset(lzma_lz_decoder *lz, lzma_allocator *allocator,
 	lz->pos = 0;
 	lz->start = 0;
 	lz->end = dict_real_size;
+	lz->dict[dict_real_size - 1] = 0;
 	lz->is_full = false;
 	lz->eopm_detected = false;
 	lz->next_finished = false;
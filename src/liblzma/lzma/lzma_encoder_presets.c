075257ab0416a0603be930082e31a5703e4ba345
Lasse Collin <lasse.collin@tukaani.org>
2010-09-26 18:10:31 +0300

    Fix the preset -3e.
    depth=0 was missing.

--- src/liblzma/lzma/lzma_encoder_presets.c
+++ src/liblzma/lzma/lzma_encoder_presets.c
@@ -50,6 +50,7 @@ lzma_lzma_preset(lzma_options_lzma *options, uint32_t preset)
 		options->mf = LZMA_MF_BT4;
 		if (level == 3 || level == 5) {
 			options->nice_len = 192;
+			options->depth = 0;
 		} else {
 			options->nice_len = 273;
 			options->depth = 512;

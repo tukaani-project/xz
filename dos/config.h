5846aeda05972bc803c6094821ae836229ebe691
Lasse Collin <lasse.collin@tukaani.org>
2022-11-09 14:57:48 +0200

    DOS: Update Makefile and config.h to include ARM64 and .lz support.

--- dos/config.h
+++ dos/config.h
@@ -16,6 +16,9 @@
 /* Define to 1 if arm decoder is enabled. */
 #define HAVE_DECODER_ARM 1
 
+/* Define to 1 if arm64 decoder is enabled. */
+#define HAVE_DECODER_ARM64 1
+
 /* Define to 1 if armthumb decoder is enabled. */
 #define HAVE_DECODER_ARMTHUMB 1
 
@@ -46,6 +49,9 @@
 /* Define to 1 if arm encoder is enabled. */
 #define HAVE_ENCODER_ARM 1
 
+/* Define to 1 if arm64 encoder is enabled. */
+#define HAVE_ENCODER_ARM64 1
+
 /* Define to 1 if armthumb encoder is enabled. */
 #define HAVE_ENCODER_ARMTHUMB 1
 
@@ -75,6 +81,9 @@
 
 /* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
+
+/* Define to 1 if .lz (lzip) decompression support is enabled. */
+#define HAVE_LZIP_DECODER 1
 
 /* Define to 1 to enable bt2 match finder. */
 #define HAVE_MF_BT2 1

d0e5ec3212dc0ef307c0bcaec28e84185a551b1d
Lasse Collin <lasse.collin@tukaani.org>
2023-01-05 21:10:32 +0200

    xz: Explicitly initialize string_to_filter_used and tweak the comment.
    Explicit init isn't needed for a static variable in this case but
    it makes it very clear what the default value must be.

--- src/xz/coder.c
+++ src/xz/coder.c
@@ -45,10 +45,10 @@ static uint32_t filters_count = 0;
 /// Number of the preset (0-9)
 static uint32_t preset_number = LZMA_PRESET_DEFAULT;
 
-/// Indicates if the current filter chain was set by the --filters
-/// option. The filter chain must be reset if a preset or single
-/// filter option is used.
-static bool string_to_filter_used;
+/// True if the current filter chain was set using the --filters option.
+/// The filter chain is reset if a preset option (like -9) or an old-style
+/// filter option (like --lzma2) is used after a --filters option.
+static bool string_to_filter_used = false;
 
 /// Integrity check type
 static lzma_check check;

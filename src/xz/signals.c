682563ec8c4297b9a4f66ba7d9bfc694ff9d7dba
Jia Tan <jiat0218@gmail.com>
2023-01-27 00:31:25 +0800

    xz: Reduce possible window of signal race condition.
    If the SIGTSTP signal is sent twice at the exact wrong time, an extra
    continue signal is needed for the process to continue.

--- src/xz/signals.c
+++ src/xz/signals.c
@@ -216,15 +216,6 @@ static void
 terminal_stop_signal_handler(int sig lzma_attribute((__unused__)))
 {
 	terminal_stop_requested = true;
-	// Unblock SIGTSTP in case it was blocked.
-	// Since it is being removed from the hooked_signals, it would
-	// not be unblocked in the next call to signals_unblock()
-	sigset_t signal_stop_set;
-	sigemptyset(&signal_stop_set);
-	sigaddset(&signal_stop_set, SIGTSTP);
-	mythread_sigmask(SIG_UNBLOCK, &signal_stop_set, NULL);
-
-	sigdelset(&hooked_signals, SIGTSTP);
 	return;
 }
 
@@ -232,23 +223,42 @@ terminal_stop_signal_handler(int sig lzma_attribute((__unused__)))
 extern void
 reset_terminal_stop_handler(void)
 {
-	// Use kill() because raise() may not be portable.
+	struct sigaction my_sa;
+	my_sa.sa_flags = 0;
+	sigemptyset(&my_sa.sa_mask);
+
 	if (terminal_stop_requested) {
+		// Unblock SIGTSTP in case it was blocked.
+		// Since it is being removed from the hooked_signals, it would
+		// not be unblocked in the next call to signals_unblock()
+		sigset_t signal_stop_set;
+		sigemptyset(&signal_stop_set);
+		sigaddset(&signal_stop_set, SIGTSTP);
+		mythread_sigmask(SIG_UNBLOCK, &signal_stop_set, NULL);
+
+		sigdelset(&hooked_signals, SIGTSTP);
+
+		// Reset the signal handler to the default so we can re-raise
+		// the SIGTSTP signal.
+		my_sa.sa_handler = SIG_DFL;
+		if (sigaction(SIGTSTP, &my_sa, NULL))
+			message_signal_handler();
+
+		// Small chance of signal race condition:
+		// If a SIGTSTP signal is caught here, we will restop
+		// the process. This is unlikely to happen in practice
+		// and the workaround is to send another signal to continue.
+
+		// Use kill() because raise() may not be portable.
 		if (kill(getpid(), SIGTSTP) != 0)
 			message_warning(_("Cannot pause process: %s"),
 					strerror(errno));
+
 		// Reset the flag after the process continues
 		terminal_stop_requested = false;
 	}
 
-	struct sigaction my_sa;
-	// Use the SA_RESETHAND flag so the SIGTSTP signal is only caught
-	// one time. This way, we can use the default signal handler to
-	// pause the process after the timing code is paused.
-	my_sa.sa_flags = (int)SA_RESETHAND;
 	my_sa.sa_handler = &terminal_stop_signal_handler;
-	sigemptyset(&my_sa.sa_mask);
-
 	sigaddset(&hooked_signals, SIGTSTP);
 
 	if (sigaction(SIGTSTP, &my_sa, NULL))

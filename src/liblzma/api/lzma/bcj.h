f029daea39c215fd7d5cb6b6798818b055cf5b22
Jia Tan <jiat0218@gmail.com>
2023-02-17 00:54:33 +0800

    liblzma: Adjust spacing in doc headers in bcj.h.

--- src/liblzma/api/lzma/bcj.h
+++ src/liblzma/api/lzma/bcj.h
@@ -21,37 +21,37 @@
 /* Filter IDs for lzma_filter.id */
 
 /**
- * \brief Filter for x86 binaries
+ * \brief       Filter for x86 binaries
  */
 #define LZMA_FILTER_X86         LZMA_VLI_C(0x04)
 
 /**
- * \brief Filter for Big endian PowerPC binaries
+ * \brief       Filter for Big endian PowerPC binaries
  */
 #define LZMA_FILTER_POWERPC     LZMA_VLI_C(0x05)
 
 /**
- * \brief Filter for IA-64 (Itanium) binaries
+ * \brief       Filter for IA-64 (Itanium) binaries
  */
 #define LZMA_FILTER_IA64        LZMA_VLI_C(0x06)
 
 /**
- * \brief Filter for ARM binaries
+ * \brief       Filter for ARM binaries
  */
 #define LZMA_FILTER_ARM         LZMA_VLI_C(0x07)
 
 /**
- * \brief Filter for ARM-Thumb binaries
+ * \brief       Filter for ARM-Thumb binaries
  */
 #define LZMA_FILTER_ARMTHUMB    LZMA_VLI_C(0x08)
 
 /**
- * \brief Filter for SPARC binaries
+ * \brief       Filter for SPARC binaries
  */
 #define LZMA_FILTER_SPARC       LZMA_VLI_C(0x09)
 
 /**
- * \brief Filter for ARM64 binaries
+ * \brief       Filter for ARM64 binaries
  */
 #define LZMA_FILTER_ARM64       LZMA_VLI_C(0x0A)
 

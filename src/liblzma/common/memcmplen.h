40392c19f71985852d75997f109dea97177d6f3f
Lasse Collin <lasse.collin@tukaani.org>
2023-07-18 13:49:43 +0300

    liblzma: Omit unnecessary parenthesis in a preprocessor directive.

--- src/liblzma/common/memcmplen.h
+++ src/liblzma/common/memcmplen.h
@@ -23,9 +23,9 @@
 // on Windows when using an MSVC compatible compiler. The Intel compiler
 // can use the intrinsics without the header file.
 #if defined(TUKLIB_FAST_UNALIGNED_ACCESS) \
-		&& (defined(_MSC_VER) \
+		&& defined(_MSC_VER) \
 		&& defined(_M_X64) \
-		&& !defined(__INTEL_COMPILER))
+		&& !defined(__INTEL_COMPILER)
 #	include <intrin.h>
 #endif
 

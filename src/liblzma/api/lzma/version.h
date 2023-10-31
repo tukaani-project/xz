49053c0a649f4c8bd2b8d97ce915f401fbc0f3d9
Jia Tan <jiat0218@gmail.com>
2023-10-31 22:30:29 +0800

    Bump version and soname for 5.4.5.

--- src/liblzma/api/lzma/version.h
+++ src/liblzma/api/lzma/version.h
@@ -23,7 +23,7 @@
 #define LZMA_VERSION_MINOR 4
 
 /** \brief Patch version number of the liblzma release. */
-#define LZMA_VERSION_PATCH 4
+#define LZMA_VERSION_PATCH 5
 
 /**
  * \brief Version stability marker

924fdeedf48113fb1e0646d86bd89a356d21a055
Lasse Collin <lasse.collin@tukaani.org>
2024-02-14 19:46:11 +0200

    liblzma: Fix validate_map.sh.
    Adding the SPDX license identifier changed the line numbers.

--- src/liblzma/validate_map.sh
+++ src/liblzma/validate_map.sh
@@ -122,7 +122,7 @@ DUPS=$(sort liblzma_generic.map | sed '/^$/d;/^global:$/d' | uniq -d)
 # ignored (@XZ_5.1.2alpha or @XZ_5.2.2 won't be added at all when
 # the #define HAVE_SYMBOL_VERSIONS_LINUX isn't used).
 IN_SYNC=
-if ! sed '109,123d' liblzma_linux.map \
+if ! sed '111,125d' liblzma_linux.map \
 		| cmp -s - liblzma_generic.map; then
 	IN_SYNC=no
 fi

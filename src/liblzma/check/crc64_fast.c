4e1c97052b5f14f4d6dda99d12cbbd01e66e3712
Jia Tan <jiat0218@gmail.com>
2024-03-05 00:34:46 +0800

    liblzma: Use attribute no_profile_instrument_function with ifunc.
    Thanks to Sam James for determining this was the attribute needed to
    workaround the GCC bug and for his version of the patch in Gentoo.

--- src/liblzma/check/crc64_fast.c
+++ src/liblzma/check/crc64_fast.c
@@ -98,6 +98,9 @@ typedef uint64_t (*crc64_func_type)(
 #	pragma GCC diagnostic ignored "-Wunused-function"
 #endif
 
+#ifdef CRC_USE_IFUNC
+__attribute__((__no_profile_instrument_function__))
+#endif
 static crc64_func_type
 crc64_resolve(void)
 {

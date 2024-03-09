8c9b8b2063daa78ead9f648c2ec3c91e8615dffb
Jia Tan <jiat0218@gmail.com>
2024-03-09 09:52:32 +0800

    liblzma: Fix typos in crc32_fast.c and crc64_fast.c.

--- src/liblzma/check/crc64_fast.c
+++ src/liblzma/check/crc64_fast.c
@@ -98,12 +98,11 @@ typedef uint64_t (*crc64_func_type)(
 #	pragma GCC diagnostic ignored "-Wunused-function"
 #endif
 
-// The funcion attributes are needed for safe IFUNC resolver usage with GCC.
 lzma_resolver_attributes
 static crc64_func_type
 crc64_resolve(void)
 {
-	return  is_arch_extension_supported()
+	return is_arch_extension_supported()
 			? &crc64_arch_optimized : &crc64_generic;
 }
 

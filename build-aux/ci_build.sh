8709407a9ef8e7e8aec117879400e4dd3e227ada
Sam James <sam@gentoo.org>
2024-04-10 17:42:23 +0100

    ci: build in parallel by default

--- build-aux/ci_build.sh
+++ build-aux/ci_build.sh
@@ -48,6 +48,8 @@ DEST_DIR="$SRC_DIR/../xz_build"
 PHASE="all"
 ARTIFACTS_DIR_NAME="output"
 
+[[ -z ${CPU_COUNT} ]] && { CPU_COUNT=$(nproc 2>/dev/null || sysctl -n hw.ncpu); }
+[[ -z ${MAKEFLAGS} ]] && export MAKEFLAGS="-j${CPU_COUNT} -l${CPU_COUNT}"
 [[ -z ${CFLAGS} ]] && export CFLAGS="-O2"
 
 ###################

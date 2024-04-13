33819abbbb4267560ba58e9ce5d6c5f1b4ff2049
Sam James <sam@gentoo.org>
2024-04-14 00:31:09 +0100

    build-aux: rewrite version.sh in awk
    Solaris' sed appears broken and chokes on the old version:
    ```
    running build-aux/version.sh
    chunk 1 (sed)
    5
    7
    0
    alpha
    chunk 2 (sed + tr)
    5|7|0|alpha|
    chunk 3 (sed + tr + sed)
    chunk 4 (sed + tr + sed + tr)
    ```
    
    vs (good):
    ```
    running build-aux/version-gsed.sh
    chunk 1 (gsed)
    5
    7
    0
    alpha
    chunk 2 (gsed + tr)
    5|7|0|alpha|
    chunk 3 (gsed + tr + gsed)
    5.7.0alpha
    chunk 4 (gsed + tr + gsed + tr)
    5.7.0alpha
    ```
    
    Rather than requiring GNU sed, rewrite in POSIX awk. Thank you to Kerin
    Millar for the rewrite.
    
    Unfortunately, we cannot rely on the xpg4 POSIX versions, because of:
    """
    Only the core-os packages are guaranteed to be present on a Solaris 11
    regular installation, thus only oawk/awk and nawk are there. In particular,
    when you create a new non global zone, it contains by default the
    solaris-small-server group package so neither the xpg4 nor the gnu awk
    binaries are available.
    """
    
    As a bonus, it's IMO far more legible now. Given Recent Events, the fewer
    immediately-obvious sed expressions in the world, the better.
    
    Thanks-to: Kerin Millar <kfm@plushkava.net>

--- build-aux/version.sh
+++ build-aux/version.sh
@@ -9,14 +9,37 @@
 #############################################################################
 #
 # Author: Lasse Collin
+#         Kerin Millar
 #
 #############################################################################
 
-sed -n 's/LZMA_VERSION_STABILITY_ALPHA/alpha/
-	s/LZMA_VERSION_STABILITY_BETA/beta/
-	s/LZMA_VERSION_STABILITY_STABLE//
-	s/^#define LZMA_VERSION_[MPS][AIT][AJNT][A-Z]* //p' \
-	src/liblzma/api/lzma/version.h \
-	| tr '\n' '|' \
-	| sed 's/|/./; s/|/./; s/|//g' \
-	| tr -d '\r\n'
+# Solaris provides a legacy awk implementation as /usr/bin/awk. It also provides
+# /usr/bin/nawk, which is more or less POSIX-compliant. Be sure to use it.
+if grep Solaris /etc/release >/dev/null 2>&1; then
+	awk=nawk
+else
+	awk=awk
+fi
+
+"$awk" -f - <<'EOF'
+
+BEGIN {
+	ARGV[1] = "src/liblzma/api/lzma/version.h"
+	ARGC = 2
+	OFS = "."
+	ORS = ""
+}
+
+/^#define LZMA_VERSION_(MAJOR|MINOR|PATCH|STABILITY) / {
+	sub(/.*_/, "", $2)
+	sub(/.*_/, "", $3)
+	if ($2 != "STABILITY" || $3 != "STABLE") {
+		ver[$2] = tolower($3)
+	}
+}
+
+END {
+	print ver["MAJOR"], ver["MINOR"], ver["PATCH"] ver["STABILITY"]
+}
+
+EOF

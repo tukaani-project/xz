ff697eb154361417d94284e0c569aa08cacf9031
Lasse Collin <lasse.collin@tukaani.org>
2024-06-16 13:39:37 +0300

    liblzma: CRC CLMUL: Omit is_arch_extension_supported() when not needed
    On E2K the function compiles only due to compiler emulation but the
    function is never used. It's cleaner to omit the function when it's
    not needed even though it's a "static inline" function.
    
    Thanks to Ilya Kurdyukov.
    
    (cherry picked from commit 30a2d5d51006301a3ddab5ef1f5ff0a9d74dce6f)

--- src/liblzma/check/crc_x86_clmul.h
+++ src/liblzma/check/crc_x86_clmul.h
@@ -385,6 +385,9 @@ crc64_arch_optimized(const uint8_t *buf, size_t size, uint64_t crc)
 #endif // BUILDING_CRC64_CLMUL
 
 
+// Even though this is an inline function, compile it only when needed.
+// This way it won't appear in E2K builds at all.
+#if defined(CRC32_GENERIC) || defined(CRC64_GENERIC)
 // Inlining this function duplicates the function body in crc32_resolve() and
 // crc64_resolve(), but this is acceptable because this is a tiny function.
 static inline bool
@@ -426,3 +429,4 @@ is_arch_extension_supported(void)
 	// code as is it only reads a variable set at startup but a few bytes
 	// doesn't matter here.
 }
+#endif

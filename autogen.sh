tukaani-project/xz

commit e9be74f5b129fe8a5388d588e68b1b7f5168a310

autogen.sh: Don't generated Doxygen docs anymore

autogen.sh
@@ -17,14 +17,12 @@ ${AUTOCONF:-autoconf}
 ${AUTOHEADER:-autoheader}
 ${AUTOMAKE:-automake} -acf --foreign
 
-# Generate the translated man pages and the doxygen documentation if the
-# "po4a" and "doxygen" tools are available.
+# Generate the translated man pages if the "po4a" tool is available.
 # This is *NOT* done by "autoreconf -fi" or when "make" is run.
-# Pass --no-po4a or --no-doxygen to this script to skip these steps.
-# It can be useful when you know that po4a or doxygen aren't available and
+# Pass --no-po4a o this script to skip this step.
+# It can be useful when you know that po4a isn't available and
 # don't want autogen.sh to exit with non-zero exit status.
 generate_po4a="y"
-generate_doxygen="y"
 
 for arg in "$@"
 do
@@ -32,22 +30,12 @@ do
 		"--no-po4a")
 			generate_po4a="n"
 			;;
-
-		"--no-doxygen")
-			generate_doxygen="n"
-			;;
 	esac
 done
 
 if test "$generate_po4a" != "n"; then
 	cd po4a
 	sh update-po
-	cd ..
-fi
-
-if test "$generate_doxygen" != "n"; then
-	cd doxygen
-	sh update-doxygen
 	cd ..
 fi
 

commit 80ffa38f56657257ed4d90d76f6bd2f2bcb8163c

Build: Fix a typo in autogen.sh
Fixes: e9be74f5b129fe8a5388d588e68b1b7f5168a310
Closes: https://github.com/tukaani-project/xz/pull/141

autogen.sh
@@ -19,7 +19,7 @@ ${AUTOMAKE:-automake} -acf --foreign
 
 # Generate the translated man pages if the "po4a" tool is available.
 # This is *NOT* done by "autoreconf -fi" or when "make" is run.
-# Pass --no-po4a o this script to skip this step.
+# Pass --no-po4a to this script to skip this step.
 # It can be useful when you know that po4a isn't available and
 # don't want autogen.sh to exit with non-zero exit status.
 generate_po4a="y"
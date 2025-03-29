tukaani-project/xz

commit c0e7eaae8d6eef1e313c9d0da20ccf126ec61f38

sysdefs.h: Add alignas

src/common/sysdefs.h
@@ -156,6 +156,17 @@ typedef unsigned char _Bool;
 #	define __bool_true_false_are_defined 1
 #endif
 
+// We may need alignas from C11/C17/C23.
+#if __STDC_VERSION__ >= 202311
+	// alignas is a keyword in C23. Do nothing.
+#elif __STDC_VERSION__ >= 201112
+#	include <stdalign.h>
+#elif defined(__GNUC__) || defined(__clang__)
+#	define alignas(n) __attribute__((__aligned__(n)))
+#else
+#	define alignas(n)
+#endif
+
 #include <string.h>
 
 // Visual Studio 2013 update 2 supports only __inline, not inline.

commit 307c02ed698a69763ef1c9c0df4ff24727442118

sysdefs.h: Avoid <stdalign.h> even with C11 compilers
Oracle Developer Studio 12.6 on Solaris 10 claims C11 support in
__STDC_VERSION__ and supports _Alignas. However, <stdalign.h> is missing.
We only need alignas, so define it to _Alignas with C11/C17 compilers.
If something included <stdalign.h> later, it shouldn't cause problems.

Thanks to Ihsan Dogan for reporting the issue and testing the fix.

Fixes: c0e7eaae8d6eef1e313c9d0da20ccf126ec61f38

src/common/sysdefs.h
@@ -172,7 +172,9 @@ typedef unsigned char _Bool;
 #if __STDC_VERSION__ >= 202311
 	// alignas is a keyword in C23. Do nothing.
 #elif __STDC_VERSION__ >= 201112
-#	include <stdalign.h>
+	// Oracle Developer Studio 12.6 lacks <stdalign.h>.
+	// For simplicity, avoid the header with all C11/C17 compilers.
+#	define alignas _Alignas
 #elif defined(__GNUC__) || defined(__clang__)
 #	define alignas(n) __attribute__((__aligned__(n)))
 #else
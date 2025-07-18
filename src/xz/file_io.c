a7ac15d36054772770e8a0ca9db7335347218a5c
Lakshmi-Surekha <Lakshmi.Kovvuri@ibm.com>
2025-07-17 23:27:06 -0500

    fsync on the directory fails in AIX

--- src/xz/file_io.c
+++ src/xz/file_io.c
@@ -493,7 +493,7 @@ io_sync_dest(file_pair *pair)
 		return true;
 	}
 
-#ifndef TUKLIB_DOSLIKE
+#if !defined TUKLIB_DOSLIKE && !defined(_AIX)
 	if (fsync(pair->dir_fd)) {
 		message_error(_("%s: Synchronizing the directory of "
 				"the file failed: %s"),

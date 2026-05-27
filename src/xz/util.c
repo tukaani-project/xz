d0d380797e9f1c25c0a88ce5d4cfd092590a209d
rootvector2 <dxbnaveed.k@gmail.com>
2026-05-27 23:51:52 +0530

    xz: Mask the multiplier suffix in str_to_uint64() error message
    A similar error message in the same function was masked in the
    previous commit 7e7bb6c21cf9.
    
    Fixes: https://github.com/tukaani-project/xz/pull/222

--- src/xz/util.c
+++ src/xz/util.c
@@ -154,7 +154,7 @@ str_to_uint64(const char *name, const char *value, uint64_t min, uint64_t max)
 
 		if (multiplier == 0) {
 			message(V_ERROR, _("%s: Invalid multiplier suffix"),
-					value - 1);
+					tuklib_mask_nonprint(value - 1));
 			message_fatal(_("Valid suffixes are 'KiB' (2^10), "
 					"'MiB' (2^20), and 'GiB' (2^30)."));
 		}
